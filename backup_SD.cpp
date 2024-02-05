ce63edc6ffdc14d79460f092fc77f2eb6c5b6b88
kodykoester <105684121+kodykoester@users.noreply.github.com>
2024-02-05 12:03:55 -0600

    Update backup_SD.cpp

--- backup_SD.cpp
+++ backup_SD.cpp
@@ -2,12 +2,10 @@
 #include <TimeLib.h>
 #include <SdFat.h>
 
-//Temp Sensor Lib.
 #define DHTPIN 2
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-//SD Card Lib.
 #define SD_FAT_TYPE 1
 #define SD_CS_PIN 23
 SdFat sd;
@@ -28,39 +26,24 @@ bool stopProgram = false;  // Variable to control program exit
 void setup()
 {
   Serial.begin(115200);      // Start serial communication
-  setTime(12, 0, 0, 4, 2, 0); // Set initial date and time (12:00:00 pm, April 2)
+  setTime(6, 0, 0, 2, 23, 2024); // Set initial date and time (replace these values with your desired date and time)
 
   pinMode(pumpRelayOne, OUTPUT);
   pinMode(pumpRelayTwo, OUTPUT);
   digitalWrite(pumpRelayOne, LOW);
   digitalWrite(pumpRelayTwo, LOW);
   dht.begin();             // Sensor Start
-  sd.begin(SD_CS_PIN);
-  // Serial.print("Initializing SD card...");
-  // if (!sd.begin(SD_CS_PIN)) {
-  //   Serial.println("initialization failed!");
-  //   return;
-  // }
-  // Serial.println("initialization done.");
 
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  myFile.open("deleteTest.txt", FILE_WRITE);
-
-  // if the file opened okay, write to it:
-  if (myFile) {
-    myFile.println("hello, this really worked. !?!");
-    // close the file:
-    myFile.close();
-    Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
+  // Barebones SD initialization
+  if (!sd.begin(SD_CS_PIN))
+  {
+    Serial.println(F("SD initialization failed!"));
+    exit(EXIT_FAILURE);
   }
 }
+
 void loop()
 {
-
   unsigned long currentMillis = millis(); // Get the current time
   float h = dht.readHumidity();          // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
   float f = dht.readTemperature(true);   // Read temperature as Fahrenheit (isFahrenheit = true)
@@ -75,7 +58,8 @@ void loop()
     // Check if 7 days have passed
     if (daysCounter >= 7)
     {
-      stopProgram = true;  // Set the variable to exit the program gracefully
+      Serial.println("Program will now end and shut down gracefully.");
+      exit(EXIT_SUCCESS);  // Use exit() to end the program gracefully
     }
   }
 
@@ -85,16 +69,6 @@ void loop()
     previousEveningMillis = currentMillis; // Save the current time for the evening check
     checkTemps(h, f);                      // Run the checkTemps function in the evening
   }
-
-  // Graceful exit
-  if (stopProgram)
-  {
-    Serial.println("Program will now stop gracefully.");
-    while (true)
-    {
-      // You can add any cleanup or other tasks here if needed
-    }
-  }
 }
 
 void runPumps()
@@ -102,7 +76,30 @@ void runPumps()
   digitalWrite(pumpRelayOne, HIGH);
   digitalWrite(pumpRelayTwo, HIGH);
   Serial.println("Pumps running!");
+
+  // Read temperature and humidity
+  float h = dht.readHumidity();
+  float f = dht.readTemperature(true);
+
+  // Save data to SD card
+  myFile = sd.open("data.txt", FILE_WRITE);
+  if (myFile)
+  {
+    myFile.print("Pump Run - ");
+    myFile.print("Humidity: ");
+    myFile.print(h);
+    myFile.print("%, ");
+    myFile.print("Temperature: ");
+    myFile.println(f);
+    myFile.close();
+  }
+  else
+  {
+    Serial.println(F("Error opening file for writing"));
+  }
+
   delay(20000);
+
   digitalWrite(pumpRelayOne, LOW);  // Turn off the pumps
   digitalWrite(pumpRelayTwo, LOW);
 }
@@ -114,42 +111,21 @@ void checkTemps(float humidity, float temperature)
   Serial.print("% ");
   Serial.print(temperature);
   Serial.println(F(" °F"));
-}
 
-void printTimeAndDate() {
-  // Print hr, min, sec, month, date
-  int hr = hour();
-  int min = minute();
-  int monthVal = month();
-  int dateVal = day();
-
-  // Convert 24-hour time to 12-hour time with AM/PM
-  int ampm = 0; // 0 for AM, 1 for PM
-  if (hr >= 12) {
-    hr = hr - 12;
-    ampm = 1;
+  // Save data to SD card
+  myFile = sd.open("data.txt", FILE_WRITE);
+  if (myFile)
+  {
+    myFile.print("Evening Check - ");
+    myFile.print("Humidity: ");
+    myFile.print(humidity);
+    myFile.print("%, ");
+    myFile.print("Temperature: ");
+    myFile.println(temperature);
+    myFile.close();
   }
-  if (hr == 0) {
-    hr = 12;
+  else
+  {
+    Serial.println(F("Error opening file for writing"));
   }
-
-  // Print the formatted time and date
-  Serial.print(hr);
-  printDigits(min);
-  Serial.print((ampm == 1) ? "pm" : "am");
-  Serial.print(" ");
-  Serial.print(monthVal);
-  Serial.print("/");
-  Serial.print(dateVal);
-  Serial.println();
-}
-
-
-
-void printDigits(int digits) {
-  // Helper function to print leading zeros
-  Serial.print(":");
-  if (digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
 }
