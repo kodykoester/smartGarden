5421d43c5497e7d20a30b9da09ae5966c63de7c4
kodykoester <105684121+kodykoester@users.noreply.github.com>
2024-02-16 09:18:22 -0600

    Update and rename backup_SD.cpp to Test_File.cpp

--- Test_File.cpp
+++ Test_File.cpp
@@ -17,8 +17,8 @@ const int pumpRelayTwo = 13; // Define the pin connected to the relay
 unsigned long previousMorningMillis = 0; // Variable to store the last time the pumps were run
 unsigned long previousEveningMillis = 0; // Variable to store the last time checkTemps was called in the evening
 
-const long morningInterval = 86400000; // 24 hours in milliseconds
-const long eveningInterval = 28800000; // 8 hours in milliseconds
+const long morningInterval = 172800000; // 2 days in milliseconds
+const long eveningInterval = 28800000;  // 8 hours in milliseconds
 
 int daysCounter = 0; // Starting count for the number of days
 bool stopProgram = false;  // Variable to control program exit
@@ -26,7 +26,7 @@ bool stopProgram = false;  // Variable to control program exit
 void setup()
 {
   Serial.begin(115200);      // Start serial communication
-  setTime(6, 0, 0, 2, 23, 2024); // Set initial date and time (replace these values with your desired date and time)
+  setTime(12, 0, 0, 2, 16, 2024); // Set initial date and time (replace these values with your desired date and time)
 
   pinMode(pumpRelayOne, OUTPUT);
   pinMode(pumpRelayTwo, OUTPUT);
@@ -40,6 +40,11 @@ void setup()
     Serial.println(F("SD initialization failed!"));
     exit(EXIT_FAILURE);
   }
+
+  // Log initial temperature and humidity check
+  float initialHumidity = dht.readHumidity();
+  float initialTemperature = dht.readTemperature(true);
+  logData("Initial Check", initialHumidity, initialTemperature);
 }
 
 void loop()
@@ -48,12 +53,19 @@ void loop()
   float h = dht.readHumidity();          // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
   float f = dht.readTemperature(true);   // Read temperature as Fahrenheit (isFahrenheit = true)
 
-  // Check if a day has passed
-  if (currentMillis - previousMorningMillis >= morningInterval)
+  // Check if it's noon to start the temperature and humidity checks
+  if (hour() == 12 && minute() == 0 && second() == 0)
+  {
+    previousEveningMillis = currentMillis; // Save the current time for the evening check
+    checkTemps(h, f);                      // Run the checkTemps function at noon
+  }
+
+  // Check if two days have passed after noon
+  if (currentMillis - previousMorningMillis >= morningInterval && hour() == 12)
   {
     previousMorningMillis = currentMillis; // Save the current time
     runPumps();                            // Run the pumps function
-    daysCounter++;                        // Increment the days counter
+    daysCounter += 2;                      // Increment the days counter by 2 since we want to run pumps every two days
 
     // Check if 7 days have passed
     if (daysCounter >= 7)
@@ -100,6 +112,9 @@ void runPumps()
 
   digitalWrite(pumpRelayOne, LOW);  // Turn off the pumps
   digitalWrite(pumpRelayTwo, LOW);
+
+  // Adjust the time to run the pumps the following day at 6 am
+  setTime(6, 0, 0, day() + 1, month(), year());
 }
 
 void checkTemps(float humidity, float temperature)
@@ -139,3 +154,21 @@ void checkTemps(float humidity, float temperature)
   }
 }
 
+void logData(const char *timestamp, float humidity, float temperature)
+{
+  myFile = sd.open("data.txt", FILE_WRITE);
+  if (myFile)
+  {
+    myFile.print(timestamp);
+    myFile.print(": ");
+    myFile.print(humidity);
+    myFile.print("%, ");
+    myFile.print("Temp: ");
+    myFile.println(temperature);
+    myFile.close();
+  }
+  else
+  {
+    Serial.println(F("Error opening file for writing"));
+  }
+}
